bcbf824014c2e89e9649857afd4583406c80fec2
RamiroCyber <ramiro.dr@gmail.com>
2021-06-20 13:06:36 -0300

    retornando codigo 201 para insercoes

--- src/main/java/com/springboot/treino/recursos/UsuarioRecurso.java
+++ src/main/java/com/springboot/treino/recursos/UsuarioRecurso.java
@@ -1,5 +1,6 @@
 package com.springboot.treino.recursos;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springboot.treino.domain.Usuario;
 import com.springboot.treino.services.UsuarioService;
@@ -32,10 +34,12 @@ public class UsuarioRecurso {
 		Usuario obj = usuarioService.buscarPorId(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Usuario> inserir(@RequestBody Usuario usuario){
+	public ResponseEntity<Usuario> inserir(@RequestBody Usuario usuario) {
 		usuario = usuarioService.inserirUser(usuario);
-		return ResponseEntity.ok().body(usuario);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(usuario.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(usuario);
 	}
 }
