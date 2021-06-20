24dd5395e5405e16debbb3dc1f992f11572e2ca6
RamiroCyber <ramiro.dr@gmail.com>
2021-06-20 15:56:06 -0300

    criando metodo de atualizacao de usuario

--- src/main/java/com/springboot/treino/recursos/UsuarioRecurso.java
+++ src/main/java/com/springboot/treino/recursos/UsuarioRecurso.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,4 +44,17 @@ public class UsuarioRecurso {
 				.toUri();
 		return ResponseEntity.created(uri).body(usuario);
 	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deletar(@PathVariable Long id) {
+		usuarioService.deletarUser(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Usuario> atualizar(@PathVariable Long id, @RequestBody Usuario usuario) {
+		usuario = usuarioService.atuazlizar(id, usuario);
+		return ResponseEntity.ok().body(usuario);
+	}
+
 }
--- src/main/java/com/springboot/treino/services/UsuarioService.java
+++ src/main/java/com/springboot/treino/services/UsuarioService.java
@@ -2,6 +2,7 @@ package com.springboot.treino.services;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +27,23 @@ public class UsuarioService {
 
 	public Usuario inserirUser(Usuario usuario) {
 		return usuarioRepositorio.save(usuario);
+	}
+
+	public void deletarUser(Long id) {
+		usuarioRepositorio.deleteById(id);
+
+	}
+
+	public Usuario atuazlizar(Long id, Usuario user) {
+		Usuario usuario = usuarioRepositorio.getOne(id);
+		updateData(usuario,user);
+		return usuarioRepositorio.save(usuario);
+	}
+
+	private void updateData(Usuario usuario, Usuario user) {
+		usuario.setNome(user.getNome());
+		usuario.setTelefone(user.getTelefone());
+		usuario.setEmail(user.getEmail());
+		
 	}
 }
